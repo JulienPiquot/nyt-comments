10953abfbd6d956090ac7886d7064c33f3a5b2de
Julien Piquot <julien.piquot@xiko.fr>
2018-11-29 14:41:09 +0100

    do LSA

--- src/main/scala/NewYorkTimesComments.scala
+++ src/main/scala/NewYorkTimesComments.scala
@@ -1,32 +1,42 @@
 import java.sql.Timestamp
 import java.util.Properties
 
+import au.com.bytecode.opencsv.CSVReader
+import breeze.io.CSVReader
+
 import scala.collection.JavaConverters._
 import edu.stanford.nlp.pipeline.{CoreDocument, StanfordCoreNLP}
 import org.apache.log4j.{Level, LogManager}
 import org.apache.spark.ml.feature.{HashingTF, IDF}
 import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
 import org.apache.spark.ml.feature.{CountVectorizer, CountVectorizerModel}
-
+import org.apache.spark.mllib.linalg.distributed.RowMatrix
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.types._
 
 import scala.io.Source
 
-case class Article(articleID: String,
-                   articleWordCount: Int,
-                   byline: String,
-                   documentType: String,
-                   headline: String,
-                   keywords: String,
-                   newDesk: String,
-                   printPage: Int,
-                   pubDate: Timestamp,
-                   sectionName: String,
-                   snippet: String,
-                   source: String,
-                   typeOfMaterial: String,
-                   webUrl: String)
-
 object NewYorkTimesComments {
+
+  val customSchema = StructType(Array(
+    //StructField("abstract", StringType, true),
+    StructField("articleID", StringType, true),
+    StructField("articleWordCount", IntegerType, true),
+    StructField("byline", StringType, true),
+    StructField("documentType", StringType, true),
+    StructField("headline", StringType, true),
+    StructField("keywords", StringType, true),
+    StructField("multimedia", IntegerType, true),
+    StructField("newDesk", StringType, true),
+    StructField("printPage", IntegerType, true),
+    StructField("pubDate", TimestampType, true),
+    StructField("sectionName", StringType, true),
+    StructField("snippet", StringType, true),
+    StructField("source", StringType, true),
+    StructField("typeOfMaterial", StringType, true),
+    StructField("webURL", StringType, true)
+  ))
 
   val article2017: Seq[String] = Seq("ArticlesJan2017.csv", "ArticlesFeb2017.csv", "ArticlesMarch2017.csv", "ArticlesApril2017.csv", "ArticlesMay2017.csv")
   val article2018: Seq[String] = Seq("ArticlesJan2018.csv", "ArticlesFeb2018.csv", "ArticlesMarch2018.csv", "ArticlesApril2018.csv")
@@ -44,50 +54,86 @@ object NewYorkTimesComments {
 
   def main(args: Array[String]): Unit = {
     val articles: DataFrame = loadArticlesAsDF()
-    articles.createOrReplaceTempView("articles")
-    val headlineTokens = articles.map(row => preprocessText(row.getAs("headline")))
+    articles.printSchema()
+    println(articles.count())
+    println(articles.show(50))
+
+
+    lsa(articles, "headline", "snippet")
+
+  }
+
+  def lsa(df: DataFrame, titleCol: String, textCol: String) = {
+    val tokens: RDD[(String, Seq[String])] = df.map(row => (row.getAs[String](titleCol), preprocessText(row.getAs(textCol)))).rdd
+    val numTerms = 1000
+    val (termDocMatrix, termIds, docIds, idfs) =
+      LSAUtils.termDocumentMatrix(tokens, numTerms, sparkSession.sparkContext)
+    val mat = new RowMatrix(termDocMatrix)
+    val k = 200
+    val svd = mat.computeSVD(k, computeU = true)
+
+    val topConceptTerms = RunLSA.topTermsInTopConcepts(svd, 10, 10, termIds)
+    val topConceptDocs = RunLSA.topDocsInTopConcepts(svd, 10, 10, docIds)
+    for ((terms, docs) <- topConceptTerms.zip(topConceptDocs)) {
+      println("Concept terms: " + terms.map(_._1).mkString("|"))
+      println("Concept docs: " + docs.map(_._1).mkString("|"))
+      println()
+    }
+  }
+
+  def basicStats(articles: DataFrame): Unit = {
+    val authorsCount = articles.flatMap(row => parseAuthor(row.getAs("byline")))
+      .groupBy("value")
+      .count()
+      .orderBy(desc("count"))
+    val authorNumber = authorsCount.count()
+    println(s"number of authors : $authorNumber")
+    authorsCount.show()
+
+    val keywordsCount = articles.flatMap(row => parseKeywords(row.getAs("keywords")))
+      .filter(author => !author.isEmpty)
+      .groupBy("value")
+      .count()
+      .orderBy(desc("count"))
+    val keywordNumber = keywordsCount.count()
+    println(s"number of keyword : $keywordNumber")
+    keywordsCount.show()
+  }
+
+  def parseKeywords(text: String): Seq[String] = {
+    text.replaceAll("(^\\[)|(\\]$)", "").split("', '").map(_.replaceAll("'", "").trim)
+  }
+
+  def parseAuthor(text: String): Seq[String] = {
+    text.replaceAll("^By ", "").split(",|( and )").map(_.trim)
+  }
+
+  def tfidf(df: DataFrame): DataFrame = {
+    val tokens = df.map(row => preprocessText(row.getAs("snippet")))
 
     // compute TF
-    val cvModel: CountVectorizerModel = new CountVectorizer().setInputCol("value").setOutputCol("tf").fit(headlineTokens)
-    val headlineTF = cvModel.transform(headlineTokens)
-
-    // check TF
-    println(headlineTF.first().getAs("value"))
-    println(headlineTF.first().getAs("tf"))
+    val cvModel: CountVectorizerModel = new CountVectorizer().setInputCol("value").setOutputCol("tf").fit(tokens)
+    val headlineTF = cvModel.transform(tokens)
 
     // compute IDF
     val idf = new IDF().setInputCol("tf").setOutputCol("idf")
     val idfModel = idf.fit(headlineTF)
-    val rescaledData = idfModel.transform(headlineTF)
-
-    rescaledData.show(10)
-    print(rescaledData.schema)
-    //rescaledData.select("label", "features").show()
-
+    idfModel.transform(headlineTF)
   }
 
   def loadArticlesAsDF(): DataFrame = {
-    val schema = Encoders.product[Article].schema
+    val csv: CSVReader = new CSVReader(null, ',', "\"")
+
+    //val schema = Encoders.product[Article].schema
     sparkSession.read
       .option("header", "true")
       .option("charset", "UTF8")
       .option("delimiter", ",")
+      //.schema(customSchema)
       .option("inferSchema", value = true)
       //.csv("data/test.csv")
-      .csv("data/ArticlesJan2017.csv")
-    //.csv(articleAll.map(f => "data/" + f): _*)
-  }
-
-  def loadArticlesAsDS(): Dataset[Article] = {
-    val schema = Encoders.product[Article].schema
-    sparkSession.read
-      .option("header", "true")
-      .option("charset", "UTF8")
-      .option("delimiter", ",")
-      .schema(schema)
-      .csv("data/ArticlesJan2017.csv")
+      .csv("data/ArticlesApril2017.csv")
       //.csv(articleAll.map(f => "data/" + f): _*)
-      .as[Article]
   }
 
   def preprocessText(text: String): Seq[String] = {
--- src/test/scala/TestNewYorkTimesComments.scala
+++ src/test/scala/TestNewYorkTimesComments.scala
@@ -1,5 +1,7 @@
 object TestNewYorkTimesComments {
   def main(args: Array[String]): Unit = {
-    print(NewYorkTimesComments.preprocessText("They Can Hit a Ball 400 Feet. But Play Catch? That’s Tricky."))
+    //print(NewYorkTimesComments.preprocessText("They Can Hit a Ball 400 Feet. But Play Catch? That’s Tricky."))
+    //NewYorkTimesComments.parseAuthor("By EZRA LEVIN, LEAH GREENBERG and ANGEL PADILLA").foreach(println(_))
+    NewYorkTimesComments.parseKeywords("['Taxation', 'Trump, Donald J', 'Koch, Charles G', 'Koch, David H', 'Americans for Prosperity', 'Club for Growth', 'Koch Industries Inc', 'Republican Party', 'United States Politics and Government', 'House of Representatives', 'International Trade and World Market']").foreach(println(_))
   }
 }
